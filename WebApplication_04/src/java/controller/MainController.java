05f7ed6909399ce55679397255fb7663a3eea2d2
KOKONOI1706 <125285431+KOKONOI1706@users.noreply.github.com>
2025-01-16 08:12:51 +0700

    init

---
+++ WebApplication_04/src/java/controller/MainController.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author GIGABYTE
+ */
+@WebServlet(name = "MainController", urlPatterns = {"/MainController"})
+public class MainController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    public int GCD(int a, int b) {
+        int min = Math.min(a, b);
+        for (int i = min; i >= 1; i--) {
+            if (a % i == 0 && b % i == 0) {
+                return i;
+            }
+        }
+        return 1;
+    }
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        String txt_numa = request.getParameter("numa");
+        String txt_numb = request.getParameter("numb");
+        if (txt_numa == null || txt_numa.trim().isEmpty()) {
+            out.println("Vui lòng nhập giá trị cho A");
+            return;
+        }
+        if (txt_numb == null || txt_numb.trim().isEmpty()) {
+            out.println("Vui lòng nhập giá trị cho B");
+            return;
+        }
+
+        int a = 0, b = 0;
+        try {
+            a = Integer.parseInt(txt_numa);
+            b = Integer.parseInt(txt_numb);
+
+            if (a <= 0 || b <= 0) {
+                out.print("A and B must be greater than 0!!");
+                return;
+            }
+        } catch (Exception e) {
+            out.println("A and B must be integer!!");
+        }
+        int result = GCD(a, b);
+        out.println("Greatest common divisor(" + a + ", " + b + ") = " + result);
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
