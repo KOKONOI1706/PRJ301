fde401c7e8ba57d9dcdb590e883ab6f99a6cd68b
KOKONOI1706 <125285431+KOKONOI1706@users.noreply.github.com>
2025-01-19 16:27:55 +0700

    test

--- CourseraLearning/src/test/ModuleQuiz_M3L3.java
+++ CourseraLearning/src/test/ModuleQuiz_M3L3.java
@@ -14,8 +14,25 @@ class Child extends Parent {
 
 public class ModuleQuiz_M3L3 {
     public static void main(String[] args) {
-        Child child = new Child();
-        child.printSuperFields();
-
+        char grade ;
+        int score = 89;
+        switch (score / 10) {
+            case 10:
+            case 9:
+                grade = 'A';
+                break;
+            case 8:
+                grade = 'B';
+                break;
+            case 7:
+                grade = 'C';
+                break;
+            case 6:
+                grade = 'D';
+                break;
+            default:
+                grade = 'F';
+        }
+        System.out.println(grade);
     }
 }
