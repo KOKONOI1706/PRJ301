298c2e3e627dc4fe26cf86a4533f4c4f5a449d93
KOKONOI1706 <125285431+KOKONOI1706@users.noreply.github.com>
2025-01-19 11:26:12 +0700

    CreatingZoo

---
+++ CreatingZoo/home/coder/coursera/creating-a-zoo/src/Dolphin.java
@@ -0,0 +1,4 @@
+public abstract class Dolphin extends Animal implements Eat{
+    private String color = "";
+    private double swim_speed = 0;
+}
---
+++ CreatingZoo/home/coder/coursera/creating-a-zoo/src/Tiger.java
@@ -0,0 +1,10 @@
+public abstract class Tiger extends Animal implements Eat,Walk{
+    private int numberOfStripes = 0;
+    private double speed = 0;
+    private int soundLevelOfRor = 0;
+
+    @Override
+    public void walking() {
+
+    }
+}
