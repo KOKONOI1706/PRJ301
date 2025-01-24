575e4cb76dd46307ecba460a5116439e022abaf0
KOKONOI1706 <125285431+KOKONOI1706@users.noreply.github.com>
2025-01-24 18:45:13 +0700

    test

--- coursera/Interface solution/Main.java
+++ coursera/Interface solution/Main.java
@@ -1,6 +1,10 @@
+import org.w3c.dom.ls.LSOutput;
+
 abstract class Animal {
     abstract void move();
 }
 public class Main {
-
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
