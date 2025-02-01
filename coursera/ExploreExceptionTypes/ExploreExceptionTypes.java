0178e10b43a4a1bb5510c75009eee9448fc4d1c0
KOKONOI1706 <125285431+KOKONOI1706@users.noreply.github.com>
2025-02-01 12:48:28 +0700

    init

---
+++ coursera/ExploreExceptionTypes/ExploreExceptionTypes.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.io.FileInputStream;
+
+public class ExploreExceptionTypes {
+    public static void main(String[] args) {
+//        File file = new File("my file.txt");
+//        FileInputStream stream = new FileInputStream(file);
+
+            int number = 10/0;
+
+    }
+}
