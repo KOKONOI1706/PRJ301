4622511a5edaa73768cbfb135adc59e526be9420
KOKONOI1706 <125285431+KOKONOI1706@users.noreply.github.com>
2025-02-01 13:47:09 +0700

    init

--- coursera/try-catch/TryWithResources.java
+++ coursera/try-catch/TryWithResources.java
@@ -1,7 +1,16 @@
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class TryWithResources {
     public static void main(String[] args) {
-        try(FileInputStream fis = new FileInputStream("test.txt"))
+        try(FileInputStream fis = new FileInputStream("coursera/try-catch/test.txt")){
+            int content;
+            while((content = fis.read()) != -1){
+                System.out.print((char)content);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
