ad542553c39563d1e164b9d3c0b5312037eb56a5
KOKONOI1706 <125285431+KOKONOI1706@users.noreply.github.com>
2025-02-01 13:59:50 +0700

    init

---
+++ coursera/C3M1/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] daysofWeek = {"Monday", "Tuesday", "Wednesday", "Thursday",
+                "Friday", "Saturday", "Sunday"};
+        for(int i = 0; i < daysofWeek.length; i++){
+            System.out.println(daysofWeek[i]);
+        }
+    }
+}
