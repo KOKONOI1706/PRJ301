1e95a54ac7e47e48f5ad2871580b41b51a942da6
KOKONOI1706 <125285431+KOKONOI1706@users.noreply.github.com>
2025-02-01 14:17:34 +0700

    init

--- coursera/C3M1/Main.java
+++ coursera/C3M1/Main.java
@@ -1,9 +1,18 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        String[] daysofWeek = {"Monday", "Tuesday", "Wednesday", "Thursday",
-                "Friday", "Saturday", "Sunday"};
-        for(int i = 0; i < daysofWeek.length; i++){
-            System.out.println(daysofWeek[i]);
+// Initialize an array with 30 seats
+        int[] seats = new int[30];
+        // Fill indices 0 to 5 with 1 (Balcony)
+        Arrays.fill(seats, 0, 5, 1);
+        // Fill indices 5 to 15 with 2 (Premium)
+        Arrays.fill(seats, 5, 15, 2);
+        // Fill indices 15 to 30 with 3 (Comfort)
+        Arrays.fill(seats, 15, 30, 3);
+
+        for (int seat : seats) {
+            System.out.println(seat);
         }
     }
 }
