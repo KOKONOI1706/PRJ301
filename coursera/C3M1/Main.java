de25436de17b9ef897faed799dbc1ca15542ba8e
KOKONOI1706 <125285431+KOKONOI1706@users.noreply.github.com>
2025-02-03 17:02:06 +0700

    init

--- coursera/C3M1/Main.java
+++ coursera/C3M1/Main.java
@@ -2,17 +2,35 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-// Initialize an array with 30 seats
-        int[] seats = new int[30];
-        // Fill indices 0 to 5 with 1 (Balcony)
-        Arrays.fill(seats, 0, 5, 1);
-        // Fill indices 5 to 15 with 2 (Premium)
-        Arrays.fill(seats, 5, 15, 2);
-        // Fill indices 15 to 30 with 3 (Comfort)
-        Arrays.fill(seats, 15, 30, 3);
+        int daysInMonth[] = new int[12];
+        daysInMonth[0] = 31;
+        daysInMonth[1] = 28;
+        daysInMonth[2] = 31;
+        daysInMonth[3] = 30;
+        daysInMonth[4] = 31;
+        daysInMonth[5] = 30;
+        daysInMonth[6] = 31;
+        daysInMonth[7] = 31;
+        daysInMonth[8] = 30;
+        daysInMonth[9] = 31;
+        daysInMonth[10] = 30;
+        daysInMonth[11] = 31;
+        String monthNames[] = new String[12];
+        monthNames[0] = "January";
+        monthNames[1] = "February";
+        monthNames[2] = "March";
+        monthNames[3] = "April";
+        monthNames[4] = "May";
+        monthNames[5] = "June";
+        monthNames[6] = "July";
+        monthNames[7] = "August";
+        monthNames[8] = "September";
+        monthNames[9] = "October";
+        monthNames[10] = "November";
+        monthNames[11] = "December";
 
-        for (int seat : seats) {
-            System.out.println(seat);
+        for(int i = 0; i < daysInMonth.length; i++){
+            System.out.println(monthNames[i] + ": " + daysInMonth[i]);
         }
     }
 }
