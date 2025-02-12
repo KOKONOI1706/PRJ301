c955b269d938952b92829979a6e66e75a66e5458
KOKONOI1706 <125285431+KOKONOI1706@users.noreply.github.com>
2025-02-12 10:26:46 +0700

    init

--- WebApplication_06/src/java/dao/UserDAO.java
+++ WebApplication_06/src/java/dao/UserDAO.java
@@ -5,10 +5,88 @@
  */
 package dao;
 
+import dto.UserDTO;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import utils.DBUtils;
+
 /**
  *
  * @author GIGABYTE
  */
-public class UserDAO {
+public class UserDAO implements IDAO<UserDTO, String>{
+
+    @Override
+    public boolean create(UserDTO entity) {
+         String sql = "INSERT INTO [tblUsers] ([userID], [fullName], [roleID], [password]) "
+                + "VALUES (N'" + entity.getUserID() + "', N'" + entity.getFullName() + "', N'" + entity.getRoleID() + "', N'" + entity.getPassword() + "')";
+        try {
+            Connection conn = DBUtils.getConnection();
+            Statement st = conn.createStatement();
+            int n = st.executeUpdate(sql);
+//            if(n > 0){
+//                return true;
+//            }else {
+//                return false;
+//            }
+            return n > 0;
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+         
+    }
+
+    @Override
+    public List<UserDTO> readAll() {
+        List<UserDTO> list = new ArrayList<UserDTO>();
+        String sql = "SELECT * FROM [tbUsers]";
+        try {
+            Connection conn = DBUtils.getConnection();
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            while(rs.next()){
+                UserDTO user = new UserDTO(
+                        rs.getString("UserID"), 
+                        rs.getString("fullName"), 
+                        rs.getString("roleID"), 
+                        rs.getString("password"));
+                list.add(user);
+            }
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
+    }
+
+    @Override
+    public UserDTO readById(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean update(UserDTO entity) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean delete(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<UserDTO> search(String searchTerm) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
     
+
 }
