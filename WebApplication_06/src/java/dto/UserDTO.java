acbc09bf9a22918e5c8be43c0c16a4765c93c972
KOKONOI1706 <125285431+KOKONOI1706@users.noreply.github.com>
2025-02-12 10:12:00 +0700

    init

---
+++ WebApplication_06/src/java/dto/UserDTO.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dto;
+
+/**
+ *
+ * @author GIGABYTE
+ */
+
+public class UserDTO {
+
+    private String userID;
+    private String fullName;
+    private String roleID;
+    private String password;
+    
+}
---
+++ WebApplication_06/src/java/models/User.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author GIGABYTE
+ */
+public class User {
+    private String userID;
+    private String fullName;
+    private String address;
+    private String phone;
+
+    public User() {
+    }
+
+    public User(String userID, String fullName, String address, String phone) {
+        this.userID = userID;
+        this.fullName = fullName;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+    
+     
+}
